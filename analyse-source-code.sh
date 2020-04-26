6f97d019b5865aca8b3391e10c79158814ef4d96
Volodymyr Yahello <vyahello@gmail.com>
2020-04-26 13:05:34 +0300

    Define public interfaces on the package level

--- analyse-source-code.sh
+++ analyse-source-code.sh
@@ -3,7 +3,7 @@
 PACKAGE="urequest"
 
 
---entry-point-box() {
+entry-point-box() {
 :<<DOC
     Provides pretty-printer check box
 DOC
@@ -23,7 +23,7 @@ check-black() {
 :<<DOC
     Runs "black" code analyser
 DOC
-    --entry-point-box "black" && ( black --check ${PACKAGE} )
+    entry-point-box "black" && ( black --check ${PACKAGE} )
 }
 
 
@@ -31,7 +31,7 @@ check-flake() {
 :<<DOC
     Runs "flake8" code analysers
 DOC
-    --entry-point-box "flake" && ( flake8 ./ )
+    entry-point-box "flake" && ( flake8 ./ )
 }
 
 
@@ -39,7 +39,7 @@ check-pylint() {
 :<<DOC
     Runs "pylint" code analyser
 DOC
-    --entry-point-box "pylint" && ( pylint $(find "${PACKAGE}/") )
+    entry-point-box "pylint" && ( pylint $(find "${PACKAGE}/") )
 }
 
 
@@ -47,7 +47,7 @@ check-mypy() {
 :<<DOC
     Runs "mypy" code analyser
 DOC
-    --entry-point-box "mypy" && ( mypy --package "${PACKAGE}" )
+    entry-point-box "mypy" && ( mypy --package "${PACKAGE}" )
 }
 
 
@@ -55,7 +55,7 @@ check-docstrings() {
 :<<DOC
      Runs "pydocstyle" static documentation code style formatter
 DOC
-    --entry-point-box "pydocstyle" && ( pydocstyle --explain --count ${PACKAGE} )
+    entry-point-box "pydocstyle" && ( pydocstyle --explain --count ${PACKAGE} )
 }
 
 
@@ -63,7 +63,7 @@ check-unittests() {
 :<<DOC
     Runs unittests using "pytest" framework
 DOC
-    --entry-point-box "unitests" && pytest
+    entry-point-box "unitests" && pytest
 }
 
 
--- urequest/__init__.py
+++ urequest/__init__.py
@@ -1,6 +1,26 @@
 """Provides user-friendly HTTP client with clean objects."""
+from typing import Tuple
+from urequest.credentials import Credentials
+from urequest.response import JsonType, Response, ResponseError, safe_response
+from urequest.session import HttpSession, LoggedHttpSession, Session
+from urequest.url import Address, HttpUrl, HttpsUrl, Url
 
 __author__: str = "Volodymyr Yahello"
 __email__: str = "vyahello@gmail.com"
 __version__: str = "0.0.4"
 __license__: str = "MIT License"
+
+__all__: Tuple[str, ...] = (
+    "Credentials",
+    "Session",
+    "HttpSession",
+    "LoggedHttpSession",
+    "JsonType",
+    "Response",
+    "ResponseError",
+    "safe_response",
+    "Address",
+    "HttpUrl",
+    "HttpsUrl",
+    "Url",
+)
